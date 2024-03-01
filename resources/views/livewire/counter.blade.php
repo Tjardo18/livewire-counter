40e71a5f1135040123587c52dd832639426ce7b8
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-03-01 13:39:35 +0100

    Counter gemaakt

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button class="btn btn-success" wire:click="up"> + </button>
+    <span class="mx-2"> {{ $count }} </span>
+    <button class="btn btn-danger" wire:click="down"> - </button>
+</div>
